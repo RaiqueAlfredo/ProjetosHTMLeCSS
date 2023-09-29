c5fe1ba9e10079242f4934996299093da971402c
Raique Alfredo <142627635+RaiqueAlfredo@users.noreply.github.com>
2023-09-29 15:13:41 -0300

    Add files via upload
    Desafio de JavaScript feito no curso

---
+++ desafio/javascript/menu.js
@@ -0,0 +1,11 @@
+(function(){
+    var $html = document.querySelector('html');
+    var $btn = document.querySelector('.header-nav__hamburgger');
+
+    $html.classList.remove('no-js');
+    $html.classList.add('js');
+
+    $btn.addEventListener('click', function(){
+        $html.classList.toggle('menu-opened');
+    })
+})()
